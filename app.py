65d3d46564018cea082988167486d4349da9b8c2
Ackerley Tng <ackerleytng@gmail.com>
2018-09-09 17:28:15 +0800

    Put api together

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import falcon
 import re
 
+from wifi_list import scan_wifi_aps
+from wpa import add_config, remove_existing_config
 
-def build_error(message):
+
+def build_message(message):
     return {
         "message": message,
     }
@@ -12,35 +15,40 @@ class WifiResource(object):
     def on_get(self, req, resp):
         """Handles GET requests"""
         resp.status = falcon.HTTP_200  # This is the default status
-        resp.media = [{"a":"b"}, {"c": "d"}]
+        resp.media = scan_wifi_aps()
 
     def on_post(self, req, resp):
         info = req.media
 
         try:
-            print(info.ssid)
-            print(info.psk)
+            add_config(info["ssid"], info["psk"])
         except AttributeError as e:
             m = re.search("no attribute '(.*?)'", e.message)
             if m:
                 resp.status = falcon.HTTP_400
-                resp.media = build_error("{} is missing".format(m.group(1)))
+                resp.media = build_message("{} is missing".format(m.group(1)))
             else:
                 raise e
+
+    def on_delete(self, req, resp):
+        remove_existing_config()
+
+        resp.status = falcon.HTTP_200
+        resp.media = build_message("Removed wifi config")
 
 
 class RequireJSON(object):
     def process_request(self, req, resp):
         if not req.client_accepts_json:
             raise falcon.HTTPNotAcceptable(
-                'This API only supports responses encoded as JSON.',
-                href='https://json.org/example.html')
+                "This API only supports responses encoded as JSON.",
+                href="https://json.org/example.html")
 
-        if req.method in ('POST', 'PUT'):
-            if 'application/json' not in req.content_type:
+        if req.method in ("POST", "PUT"):
+            if "application/json" not in req.content_type:
                 raise falcon.HTTPUnsupportedMediaType(
-                    'This API only supports requests encoded as JSON.',
-                    href='https://json.org/example.html')
+                    "This API only supports requests encoded as JSON.",
+                    href="https://json.org/example.html")
 
 
 app = falcon.API(middleware=[
@@ -48,4 +56,4 @@ app = falcon.API(middleware=[
 ])
 
 wifi = WifiResource()
-app.add_route('/wifi', wifi)
+app.add_route("/wifi", wifi)
